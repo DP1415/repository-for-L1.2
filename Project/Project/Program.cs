b6e5bb940251dd46e9dfe6a53fb117b9b330861f
DP1415 <Daniil-2005-zima@mail.ru>
2024-02-17 10:07:27 +0300

    3 V

--- Project/Project/Program.cs
+++ Project/Project/Program.cs
@@ -8,6 +8,7 @@
             Console.WriteLine("Хоть бы вышло");
             Console.WriteLine("Введите число больше 5, но меньше 100");
             int a = Convert.ToInt32(Console.ReadLine());
+            string str = "a > 5 - " + Convert.ToString(a > 5) + " & a < 100 - " + Convert.ToString(a < 100);
         }
     }
 }
