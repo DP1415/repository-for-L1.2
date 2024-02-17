e028734da4d4c256bdc45c3d3c6a5eb8ae283a38
Denis <phenomenal478@gmail.com>
2024-02-17 10:10:01 +0300

    Initial commit

--- Project/Project/Program.cs
+++ Project/Project/Program.cs
@@ -9,6 +9,7 @@
             Console.WriteLine("Введите число больше 5, но меньше 100");
             int a = Convert.ToInt32(Console.ReadLine());
             string str = "a > 5 - " + Convert.ToString(a > 5) + " & a < 100 - " + Convert.ToString(a < 100);
+            Console.WriteLine(str);
         }
     }
 }
